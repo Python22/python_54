273ce75196bfafe4e70fd6b570441e67057ffaa9
Python22 <Aleksandrvalerievichshag@gmail.com>
2026-08-05 21:53:17 +0200

    03

--- web_lessons/python_54_flask_01/app.py
+++ web_lessons/python_54_flask_01/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for       # подключаем фласк и его компоненты
-from flask_sqlalchemy import SQLAlchemy                 # для взаимодействия с БД и работой с таблицами из БД    
-from flask_migrate import Migrate                       # класс для создания/изменения таблиц в БД
+from flask import Flask, render_template, url_for, request, redirect        # подключаем фласк и его компоненты
+from flask_sqlalchemy import SQLAlchemy                                     # для взаимодействия с БД и работой с таблицами из БД    
+from flask_migrate import Migrate                                           # класс для создания/изменения таблиц в БД
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 
 
@@ -50,9 +50,29 @@ def check_answer():
 @app.route("/multiplayer") 
 def multiplayer():
     return render_template(template_name_or_list="multiplayer.html")
-@app.route("/registration") 
+
+
+@app.route("/registration", methods=["POST", "GET"]) 
 def registration():
+    if request.method == "POST":                # если данные от пользователя пришли с методом POST, то это значит пришли данные из формы регистрации
+        username = request.form["username"]     # что пришло из input с именем username
+        password = request.form["password"]     # что пришло из input с именем password
+        password2 = request.form["password2"]   # что пришло из input с именем password2
+        print(f"Кто-то хочет создать аккаунт/// username: {username}, {password}, {password2}")
+
+        error_messages = []                     # если данные не проходят валидацию, то тут будем писать что не так и потом отправим это клиенту
+
+        if username is None or password is None or password2 is None:       # если что-то из полей не пришло
+            error_messages.append("Все поля обязательны;")                   # добавляем в список ошибок такое сообщение
+        if len(username) > 50:
+            error_messages.append("Ник слишком длинный, должен быть до 50 символов;")    
+        if password != password2:
+            error_messages.append("Пароли должны совпадать;")
+        print(f"errors: {error_messages}")        
+
     return render_template(template_name_or_list="registration.html")
+
+
 @app.route("/login") 
 def login():
     return render_template(template_name_or_list="login.html")
